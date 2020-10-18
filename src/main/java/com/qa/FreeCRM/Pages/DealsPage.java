808eb4490a00e92d5b9f713b875846b69583987a
NayanaDarshan <nayana.28mr@gmail.com>
2020-10-18 10:13:22 +0530

    Added the deals page

---
+++ src/main/java/com/qa/FreeCRM/Pages/DealsPage.java
@@ -0,0 +1,12 @@
+package com.qa.FreeCRM.Pages;
+
+import com.qa.FreeCRM.base.BasePage;
+
+public class DealsPage extends BasePage{
+	
+  
+	public DealsPage() {
+		 System.out.println("Added the Deals Page constructor");
+	}
+
+}
